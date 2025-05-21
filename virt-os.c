649e0b1d8344952fe0539bf952b58faecf4645bc
cinco-05 <128838569+cinco-05@users.noreply.github.com>
2025-05-20 20:17:14 -0500

    Create virt-os.c

---
+++ virt-os.c
@@ -0,0 +1,33 @@
+//CJ Sturiale
+#include <stdio.h>
+#include "virt-os.h"
+
+// Dummy versions of the real functions
+void initializeOS() {
+    printf("initializeOS() called\n");
+}
+
+void registerKeyPressCallback(key_press_callback_t callback) {
+    printf("registerKeyPressCallback() called\n");
+
+    // Simulate a user typing "echo Hello", then pressing Enter
+    callback('e');
+    callback('c');
+    callback('h');
+    callback('o');
+    callback(' ');
+    callback('H');
+    callback('e');
+    callback('l');
+    callback('l');
+    callback('o');
+    callback(13);  // Enter
+}
+
+void registerCommandHandler(command_handler_callback_t callback) {
+    printf("registerCommandHandler() called\n");
+}
+
+void insertKeyData(const char *data) {
+    printf("[DISPLAY]: %s", data);
+}
